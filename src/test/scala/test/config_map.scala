a7e54477e2a6e37dca4119efb191d4d2d1bf1f15
杨颉 <erwin.yang@chinapex.com>
2021-04-21 18:05:11 +0800

    token

--- src/test/scala/test/config_map.scala
+++ src/test/scala/test/config_map.scala
@@ -1,6 +1,8 @@
 package test
 //import com.DC_test.demo.scala.GetConfig
+import com.DC_test.demo.scala.Http.{getToken, sendPost}
 import org.yaml.snakeyaml.Yaml
+
 import java.util
 
 object config_map {
@@ -15,7 +17,8 @@ object config_map {
   val repeat      = obj.get("repeat").asInstanceOf[Int]
   val during      = obj.get("during").asInstanceOf[Int]
   val user      = obj.get("user").asInstanceOf[Int]
-
+  val resp:String = sendPost("https://apex-dc-api-test.chinapex.com.cn/signature/password_token", "{\"app_id\": \"18e94303355ed4e7fe20f928cb663f99c7de0905\", \"app_secret\": \"ec49291da062b806d721d18c1c735ea900d1f66b\"}");
+  val access_token:String = getToken(resp)
 
   //val test = "https://apex-dc-push-test.chinapex.com.cn"
   //val map = Map("test"->"   ".toByte)
